e044ad931b79c752d4b38de1607cd17594093ac0
Frosty <joeyfrost007@gmail.com>
2020-09-26 18:36:25 -0400

    placePiece4A method. Not fully javadoced.

--- src/v01/Solver.java
+++ src/v01/Solver.java
@@ -62,7 +62,6 @@ public class Solver {
 		return newPuzzle;
 	}
 	
-<<<<<<< HEAD
 	/**
 	 * 
 	 * 
@@ -77,62 +76,76 @@ public class Solver {
 		boolean valid = true;
 		int height = puzzle.getHeight();
 		int width = puzzle.getWidth();
-
-		/* Check that the starting positions are not null. */
-		valid = checkNullStart(startX, startY, puzzle);
+		int leftBoundIndex = startX-1;
+		int topBoundIndex = startY+2;
+		int rightBoundIndex = startX+1;
+		int botBoundIndex = startY;
+		int x = startX;
+		int y = startY;
 		
-		/* Check if the piece can be placed. */
-		/* Bottom left */
-		if(board[startY][startX] != 0) { //check for position blocked
-			valid = false;
+		/* Check if the piece can be placed.
+		   1st check if the piece will ever go out of bounds. */
+		/* The x-axis width */
+		for(int i = leftBoundIndex; valid && i < rightBoundIndex; i++) {
+			if(i < 0 || i >= width) {
+				valid = false;
+			}
 		}
-		/* Bottom right */
-		if(valid && (startX+1) >= width) { //check for out of bounds
-			valid = false;
+		/* The y-axis height */
+		for(int i = topBoundIndex; valid && i < botBoundIndex; i++) {
+			if(i < 0 || i >= height) {
+				valid = false;
+			}
 		}
-		else if(board[startY][startX+1] != 0) { //check for position blocked
-			valid = false;
-		}
-		/* Mid left */
-		if(valid && ((startX-1) < 0 || (startY-1) < 0 ) ) { //check for out of bounds
-			valid = false;
-		}
-		else if(board[startY-1][startX-1] != 0) { //check for position blocked
-			valid = false;
-		}
-		/* Mid right */
-		if() { //check for out of bounds
-
-		}
-		else if() { //check for position blocked
-
-		}
-		/* Top left */
-		if() { //check for out of bounds
-
-		}
-		else if() { //check for position blocked
-
-		}
-		/* Top right */
-		if() { //check for out of bounds
-
-		}
-		else if() { //check for position blocked
-
-		}
-		
-		/* Place the piece. */
+		/* 2nd check if the locations of the piece are occupied. */
 		if(valid) {
+			/* Bottom left */
+			if(board[y][x] != 0) {
+				valid = false;
+			}
+			/* Bottom right */
+			if(valid && board[y][x+1] != 0) {
+				valid = false;
+			}
+			/* Mid left */
+			if(valid && board[y-1][x-1] != 0) {
+				valid = false;
+			}
+			/* Mid right */
+			if(valid && board[y-1][x] != 0) {
+				valid = false;
+			}
+			/* Top left */
+			if(valid && board[y-2][x] != 0) {
+				valid = false;
+			}
+			/* Top right */
+			if(valid && board[y-2][x+1] != 0) {
+				valid = false;
+			}
+		}
 
+		/* If the location of the piece is valid, 
+		 * place the piece. */
+		if(valid) {
+			/* Bottom left */
+			board[y][x] = 4;
+			/* Bottom right */
+			board[y][x+1] = 4;
+			/* Mid left */
+			board[y-1][x-1] = 4;
+			/* Mid right */
+			board[y-1][x] = 4;
+			/* Top left */
+			board[y-2][x] = 4;
+			/* Top right */
+			board[y-2][x+1] = 4;
 		}
 		
 		newPuzzle.setBoard(board);
 		return newPuzzle;
 	}
-=======
-	//piece2A
->>>>>>> branch 'master' of https://github.com/jiangs11/BrickPuzzleSolver.git
+
 	
 	private boolean checkNullStart(int startX, int startY, Puzzle puzzle) {
 		boolean valid = true;
