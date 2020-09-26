1d67d52cd945df9c69dd477340e9141299bed297
Frosty <joeyfrost007@gmail.com>
2020-09-26 16:05:16 -0400

    Changed buildPiece to placePiece. Made a new class called Solver. This class will solve the puzzle. placePiece methods should be in Solver instead of Driver, bc it is an action of Solver.

--- src/v01/Driver.java
+++ src/v01/Driver.java
@@ -3,12 +3,13 @@ package v01;
 public class Driver {
 
 	public static void main(String[] args) {
-		Puzzle puzzle1 = new Puzzle(6,6);
-		puzzle1.setBoard(buildPuzzle1(puzzle1.getBoard()));
-		//displayPuzzle(puzzle1);
+		Puzzle puzzle2 = new Puzzle(6,6);
+		Solver solver = new Solver();
 		
-		puzzle1 = buildPiece1(3,1, puzzle1);
-		displayPuzzle(puzzle1);
+		puzzle2.setBoard(buildPuzzle2(puzzle2.getBoard()));
+		
+		
+		displayPuzzle(puzzle2);
 	}
 	
 	/**
@@ -38,7 +39,7 @@ public class Driver {
 	 * @param puzzle The puzzle being built.
 	 * @return The board of the puzzle being built.
 	 */
-	private static int[][] buildPuzzle1(int[][] board) {
+	private static int[][] buildPuzzle2(int[][] board) {
 		int[][] newBoard = board;
 		
 		newBoard[0][0] = 9;
@@ -51,61 +52,7 @@ public class Driver {
 		return newBoard;
 	}
 	
-	/**
-	 * 
-	 * 
-	 * @param startX
-	 * @param startY
-	 * @param puzzle
-	 * @return
-	 */
-	private static Puzzle buildPiece1(int startX, int startY, Puzzle puzzle) {
-		Puzzle newPuzzle = puzzle;
-		int[][] board = newPuzzle.getBoard();
-		boolean valid = true;
-		int height = puzzle.getHeight();
-		int width = puzzle.getWidth();
-		
-		/* Check that the starting positions are not null. */
-		valid = checkNullStart(startX, startY, puzzle);
-		
-		/* Check if the piece can be placed. */
-		for(int i = startX; i < startX + 6 && valid; i++) {
-			/* Check for nulls. */
-			if(i >= width) {
-				valid = false;
-				break;
-			}
-			/* Check to see if the positions are not blocked. */
-			if(board[startY][i] != 0) {
-				valid = false;
-			}
-		}
-		
-		/* Place the piece. */
-		if(valid) {
-			for(int i = startX; i < startX + 6; i++) {
-				board[startY][i] = 1;
-			}
-		}
-
-		return newPuzzle;
-	}
 	
-	private static boolean checkNullStart(int startX, int startY, Puzzle puzzle) {
-		boolean valid = true;
-		int height = puzzle.getHeight();
-		int width = puzzle.getWidth();
-		
-		/* Check that the starting positions are not null. */
-		if((startX < 0 || startY < 0) && 
-				(startY >= height ||
-						startX >= width)) {
-			valid = false;
-		}
-		
-		return valid;
-	}
 
 }
 
---
+++ src/v01/Solver.java
@@ -0,0 +1,79 @@
+package v01;
+
+/**
+ * Solves the brick puzzle. 
+ * 
+ * @author Joseph Frost
+ * @version 2020.09.26
+ */
+public class Solver {
+	
+	public Solver() {
+		
+	}
+	
+	public Puzzle solve(Puzzle puzzle) {
+		Puzzle solved = puzzle;
+		int[][] board = solved.getBoard();
+		
+		
+		
+		return solved;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @param startX
+	 * @param startY
+	 * @param puzzle
+	 * @return
+	 */
+	private Puzzle placePiece1(int startX, int startY, Puzzle puzzle) {
+		Puzzle newPuzzle = puzzle;
+		int[][] board = newPuzzle.getBoard();
+		boolean valid = true;
+		int height = puzzle.getHeight();
+		int width = puzzle.getWidth();
+		
+		/* Check that the starting positions are not null. */
+		valid = checkNullStart(startX, startY, puzzle);
+		
+		/* Check if the piece can be placed. */
+		for(int i = startX; i < startX + 6 && valid; i++) {
+			/* Check for nulls. */
+			if(i >= width) {
+				valid = false;
+				break;
+			}
+			/* Check to see if the positions are not blocked. */
+			if(board[startY][i] != 0) {
+				valid = false;
+			}
+		}
+		
+		/* Place the piece. */
+		if(valid) {
+			for(int i = startX; i < startX + 6; i++) {
+				board[startY][i] = 1;
+			}
+		}
+
+		return newPuzzle;
+	}
+	
+	private boolean checkNullStart(int startX, int startY, Puzzle puzzle) {
+		boolean valid = true;
+		int height = puzzle.getHeight();
+		int width = puzzle.getWidth();
+		
+		/* Check that the starting positions are not null. */
+		if((startX < 0 || startY < 0) && 
+				(startY >= height ||
+						startX >= width)) {
+			valid = false;
+		}
+		
+		return valid;
+	}
+}
