4a74c21b99e4587927cbf981d01ca83d6e073e56
Steven Jiang <jiangs1@students.rowan.edu>
2020-09-27 16:40:46 -0400

    Delete BrickPuzzleSolver.java

--- BrickPuzzleSolver.java
+++
@@ -1,8 +0,0 @@
-
-public class BrickPuzzleSolver {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        int[][] puzzle = { {},
-                         };
-    }
-}
